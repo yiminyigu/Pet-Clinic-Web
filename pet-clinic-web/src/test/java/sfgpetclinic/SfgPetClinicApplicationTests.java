da35de32a541fffc9bf2267ef5d9fd45c45331ff
yiminyigu <739343896@qq.com>
2019-12-26 16:49:21 -0500

    maven plugin release

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,9 +1,14 @@
 package sfgpetclinic;
 
+import guru.framework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SfgPetClinicApplication.class)
 class SfgPetClinicApplicationTests {
 
     @Test
