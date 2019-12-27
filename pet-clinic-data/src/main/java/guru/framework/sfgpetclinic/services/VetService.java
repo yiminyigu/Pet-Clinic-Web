2db35ac9440cfee68f777118e8509a47afdf3abc
yiminyigu <739343896@qq.com>
2019-12-26 20:28:08 -0500

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package guru.framework.sfgpetclinic.services;
+
+import guru.framework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
