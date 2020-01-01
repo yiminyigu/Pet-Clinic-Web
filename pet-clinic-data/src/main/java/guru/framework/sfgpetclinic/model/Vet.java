8ffb83490938b99e8ab159f12462cc28e20d1103
yiminyigu <739343896@qq.com>
2020-01-01 10:19:42 -0500

    Updated vets and specialities with JPA annotations. Closes #37

--- pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,14 @@
 package guru.framework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_speciality",joinColumns =@JoinColumn(name = "vet_id"),inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities=new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
