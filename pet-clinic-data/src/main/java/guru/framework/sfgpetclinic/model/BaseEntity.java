4c4daf2db1394d1fa37b1c07447b9353b417b54a
yiminyigu <739343896@qq.com>
2020-01-01 22:59:40 -0500

    added lombok. closes #51

--- pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/BaseEntity.java
@@ -1,10 +1,18 @@
 package guru.framework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
 
 @MappedSuperclass
 public class BaseEntity implements Serializable {
@@ -12,11 +20,7 @@ public class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public BaseEntity(Long id) {
+        this.id=id;
     }
 }
--- pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/framework/sfgpetclinic/model/Person.java
@@ -1,7 +1,16 @@
 package guru.framework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
 
 @MappedSuperclass //do not create this entities
 public class Person extends BaseEntity{
@@ -10,19 +19,9 @@ public class Person extends BaseEntity{
     @Column(name = "last_name")
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName=firstName;
+        this.lastName=lastName;
     }
 }
