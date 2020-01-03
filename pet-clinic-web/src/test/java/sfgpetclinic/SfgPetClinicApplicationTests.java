074e6b7b57d7a112c8546bf6d8e7354fc55b66ee
yiminyigu <739343896@qq.com>
2020-01-03 17:42:07 -0500

    upgrade to Junit 5 closes #4

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -2,12 +2,13 @@ package sfgpetclinic;
 
 import guru.framework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = SfgPetClinicApplication.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = SfgPetClinicApplication.class)
 class SfgPetClinicApplicationTests {
 
