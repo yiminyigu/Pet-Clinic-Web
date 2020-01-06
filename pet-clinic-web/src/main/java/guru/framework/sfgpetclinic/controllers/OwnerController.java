65448e9f3f921acc5c56e29bb3a278c60cd11a20
yiminyigu <739343896@qq.com>
2020-01-05 20:14:05 -0500

    added display owner function. closes #53

--- pet-clinic-web/src/main/java/guru/framework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/framework/sfgpetclinic/controllers/OwnerController.java
@@ -4,7 +4,10 @@ import guru.framework.sfgpetclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -27,5 +30,19 @@ public class OwnerController {
     public String findOwners(){
         return "notimplemented";
     }
+
+    /**
+     * Custom handler for displaying an owner.
+     * @param ownerId the ID of the owner to display
+     * @return a ModelMap with the model attributes for the view
+     */
+    @GetMapping("/{ownerId}")
+    public ModelAndView displayOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
+
 
 }
--- pet-clinic-web/src/test/java/guru/framework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/framework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -15,7 +15,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -66,4 +67,19 @@ class OwnerControllerTest {
 
 
     }
+
+
+    @Test
+    void displayOwner() throws Exception{
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners/1"))
+                .andExpect(status().is(200))
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner",hasProperty("id",is(1l))));
+
+
+    }
+
+
 }
