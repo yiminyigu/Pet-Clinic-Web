c3bbbe340461e16d40f2ce375629c15eea5ed744
yiminyigu <739343896@qq.com>
2020-01-07 23:10:21 -0500

    test responsebody to bind return value to json. closes #66

--- pet-clinic-web/src/main/java/guru/framework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/framework/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package guru.framework.sfgpetclinic.controllers;
 
+import guru.framework.sfgpetclinic.model.Vet;
 import guru.framework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +26,12 @@ public class VetController {
 
         model.addAttribute("vets",vetService.findAll());
         return "vets/Index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
